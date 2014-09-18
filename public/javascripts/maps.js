afcf75f962bc58bd0bac0becf1bc98743aab212e
nadhub <nadir.hamaidi@gmail.com>
2014-09-18 14:51:14 +0000

    commit

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -2,8 +2,8 @@ function initialize(){
 
 		var mapOptions = {
 
-			center: new google.maps.LatLng(0,0),
-			zoom: 4
+			center: new google.maps.LatLng(47.0000,2.0000),
+			zoom: 6
 		}
 
 		var map = new google.maps.Map(document.getElementById("mapId"), mapOptions);
