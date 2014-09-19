d3fe7d51a4ad0d87448ded2a69e613143f29edd6
nadir <nadir.hamaidi@gmail.com>
2014-09-19 16:52:10 +0200

    commit

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -2,15 +2,16 @@ function initialize(){
 
 		var mapOptions = {
 
-			center: new google.maps.LatLng(47.0000,2.0000),
+			center: new google.maps.LatLng(49.0000,-8.0000),
 			zoom: 6
 		}
 
 		var map = new google.maps.Map(document.getElementById("mapId"), mapOptions);
     	
     	$("#myModal").on("shown.bs.modal", function () {
-        google.maps.event.trigger(map, "resize");
-    });
+        	google.maps.event.trigger(map, "resize");
+    	});
+
 	}
 
     google.maps.event.addDomListener(window, "load", initialize);
--- public/server/controller/mainCtrl.js
+++ public/server/controller/mainCtrl.js
@@ -1,6 +1,74 @@
-app.controller('MainCtrl', ['$scope', function($scope){
+app.run(function () {
+
+	function init($scope){
+
+		var input = document.getElementById("depart");
+
+		var mapOptions = {
+						center: new google.maps.LatLng(49.0000,-8.0000),
+						zoom: 6
+						}
+
+		var map = new google.maps.Map(document.getElementById("mapId"), mapOptions);
+    	
+    	$("#myModal").on("shown.bs.modal", function () {
+        	google.maps.event.trigger(map, "resize");
+    	});
+
+
+    	$scope.calculeRoute = function(){
+
+    		var directionsService = new google.maps.DirectionsService();
+    		var directiondisplay;
+    		directiondisplay = new google.maps.DirectionsRenderer();
+    		directiondisplay.setMap(map);
+    		directiondisplay.setPanel(document.getElementById("panel"));
+
+    		var start = document.getElementById("depart").value;
+    		var end = document.getElementById("destination").value;
+    		var request = {
+
+    			origin: start,
+    			destination : destination,
+    			travelMode : google.maps.TravelMode.DRIVING
+    		};
+
+    		directionsService.route(request, function(result, status){
+
+    			if (status == google.maps.DirectionsStatus.OK) {
+
+    				directiondisplay.setDirections(result);
+    			}else {
+    				alert("Désolé il y a eu un souci lors du calcul. Voulez-vous réessayer ");
+    			}
+    		});
+    	}
+    	
+	}
+
+    google.maps.event.addDomListener(window, "load", init);
+
+});
+
+////////////////////
+app.controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout){
+
+
+	$scope.flashMessage = false;
+	$scope.msg = "Nous recherchons le parcours le plus court pour vous...";
+
     $scope.showModal = function(){
-        $('#myModal').modal('show');        
-    }
+    	
+    	$scope.flashMessage = true; 
+
+    	$timeout(function() {
+    		$('#myModal').modal('show');   
+    		$scope.flashMessage = false;
+    
+    	}, 1000);
+
+    }	
+        
+        
     
 }]);
